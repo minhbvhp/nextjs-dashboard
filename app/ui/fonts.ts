3c06313cb3cc08808acc22e9289d12cb63236fa7
trankhoaminh <tranminhbvhp@gmail.com>
2024-04-25 16:48:32 +0700

    font Lusitana fix

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,8 +1,13 @@
-import { Inter, Dancing_Script } from 'next/font/google';
+import { Inter, Dancing_Script, Lusitana } from 'next/font/google';
 
 export const inter = Inter({ subsets: ['vietnamese'] });
 
 export const dancingScript = Dancing_Script({
   weight: ['400', '400'],
   subsets: ['vietnamese'],
+});
+
+export const lusitana = Lusitana({
+  weight: ['400', '400'],
+  subsets: ['latin'],
 });
