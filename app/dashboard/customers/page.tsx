330ce55a8d53883ae7e27c54b05dc52e37389630
trankhoaminh <tranminhbvhp@gmail.com>
2024-04-25 16:43:50 +0700

    chapter 5

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,3 @@
+export default function Page() {
+  return <p>Nội dung trang Khách hàng</p>;
+}
---
+++ app/ui/fonts.ts
@@ -0,0 +1,8 @@
+import { Inter, Dancing_Script } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['vietnamese'] });
+
+export const dancingScript = Dancing_Script({
+  weight: ['400', '400'],
+  subsets: ['vietnamese'],
+});
